89ae82e5cdb7117200698054154ad45e5bb41796
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:34 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/npthport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/npthport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/npthport.git'), string(name: 'PORT_DESCRIPTION', value: 'The New GNU Portable Threads library' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/npthport.git'), string(name: 'PORT_DESCRIPTION', value: 'The New GNU Portable Threads library' )]
   }
 }
